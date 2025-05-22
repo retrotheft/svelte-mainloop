6e4aaf34c2de9d7543b2cebdc64395915c22ce01
Jim Bridger <retrotheft@gmail.com>
2025-05-23 08:17:42 +1000

    1.2.2 - exported Loop class from index.js

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## [1.2.2] - 2025-05-23
+
+### Fixed
+   - exported `Loop` class from index.js
+
 ## [1.2.1] - 2025-05-22
 
 ### Fixed
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,8 +1,9 @@
 import JoinLoop from './components/JoinLoop.svelte';
 import ViewLoop from './components/ViewLoop.svelte';
 import loop from './loop.svelte.js';
-export type { BeginCallback, UpdateCallback, DrawCallback, EndCallback } from "./loop.svelte.js"
 
+export type { BeginCallback, UpdateCallback, DrawCallback, EndCallback } from "./loop.svelte.js"
+export { Loop } from './loop.svelte.js'
 export { JoinLoop, ViewLoop };
 
 export default loop;
